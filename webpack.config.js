664949009e79d62574627a883f0bb0b95a162d63
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-08-01 13:04:36 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env) => {
+   const isProduction = env === 'production'
+
+   return {
+      entry: './src/app.js',
+      output: {
+         path: path.join(__dirname, 'public', 'dist'),
+         filename: 'bundle.js'
+      },
+      module: {
+         rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+         }, {
+            test: /\.s?css$/,
+            use: [
+               MiniCssExtractPlugin.loader,
+               {
+                  loader: 'css-loader',
+                  options: {
+                     sourceMap: true
+                  }
+               },
+               {
+                  loader: 'sass-loader',
+                  options: {
+                     sourceMap: true
+                  }
+               }
+            ]
+         }]
+      },
+
+      plugins: [
+         new MiniCssExtractPlugin({
+            filename: "styles.css",
+         }),
+      ],
+      devtool: isProduction ? 'none' : 'inline-source-map',
+      devServer: {
+         contentBase: path.join(__dirname, 'public'),
+         historyApiFallback: true,
+         publicPath: '/dist/'
+      }
+   }
+}
+
